01c9f08453d2135aaa1917cf6aecea5adf4fb050
Priyam Harsh <priyamharsh14@gmail.com>
2020-12-10 19:26:38 +0530

    Created ChatBot Class

--- chatbot.py
+++ chatbot.py
@@ -8,88 +8,109 @@ from transformers import OpenAIGPTLMHeadModel, OpenAIGPTTokenizer, GPT2LMHeadMod
 from train import SPECIAL_TOKENS, build_input_from_segments
 
 def top_filtering(logits, top_k=0., top_p=0.9, threshold=-float('Inf'), filter_value=-float('Inf')):
-    """ Filter a distribution of logits using top-k, top-p (nucleus) and/or threshold filtering
-        Args:
-            logits: logits distribution shape (vocabulary size)
-            top_k: <=0: no filtering, >0: keep only top k tokens with highest probability.
-            top_p: <=0.0: no filtering, >0.0: keep only a subset S of candidates, where S is the smallest subset
-                whose total probability mass is greater than or equal to the threshold top_p.
-                In practice, we select the highest probability tokens whose cumulative probability mass exceeds
-                the threshold top_p.
-            threshold: a minimal threshold to keep logits
-    """
-    assert logits.dim() == 1  # Only work for batch size 1 for now - could update but it would obfuscate a bit the code
-    top_k = min(top_k, logits.size(-1))
-    if top_k > 0:
-        # Remove all tokens with a probability less than the last token in the top-k tokens
-        indices_to_remove = logits < torch.topk(logits, top_k)[
-            0][..., -1, None]
-        logits[indices_to_remove] = filter_value
+	""" Filter a distribution of logits using top-k, top-p (nucleus) and/or threshold filtering
+		Args:
+			logits: logits distribution shape (vocabulary size)
+			top_k: <=0: no filtering, >0: keep only top k tokens with highest probability.
+			top_p: <=0.0: no filtering, >0.0: keep only a subset S of candidates, where S is the smallest subset
+				whose total probability mass is greater than or equal to the threshold top_p.
+				In practice, we select the highest probability tokens whose cumulative probability mass exceeds
+				the threshold top_p.
+			threshold: a minimal threshold to keep logits
+	"""
+	assert logits.dim() == 1  # Only work for batch size 1 for now - could update but it would obfuscate a bit the code
+	top_k = min(top_k, logits.size(-1))
+	if top_k > 0:
+		# Remove all tokens with a probability less than the last token in the top-k tokens
+		indices_to_remove = logits < torch.topk(logits, top_k)[
+			0][..., -1, None]
+		logits[indices_to_remove] = filter_value
 
-    if top_p > 0.0:
-        # Compute cumulative probabilities of sorted tokens
-        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-        cumulative_probabilities = torch.cumsum(
-            F.softmax(sorted_logits, dim=-1), dim=-1)
+	if top_p > 0.0:
+		# Compute cumulative probabilities of sorted tokens
+		sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+		cumulative_probabilities = torch.cumsum(
+			F.softmax(sorted_logits, dim=-1), dim=-1)
 
-        # Remove tokens with cumulative probability above the threshold
-        sorted_indices_to_remove = cumulative_probabilities > top_p
-        # Shift the indices to the right to keep also the first token above the threshold
-        sorted_indices_to_remove[...,
-                                 1:] = sorted_indices_to_remove[..., :-1].clone()
-        sorted_indices_to_remove[..., 0] = 0
+		# Remove tokens with cumulative probability above the threshold
+		sorted_indices_to_remove = cumulative_probabilities > top_p
+		# Shift the indices to the right to keep also the first token above the threshold
+		sorted_indices_to_remove[...,
+								 1:] = sorted_indices_to_remove[..., :-1].clone()
+		sorted_indices_to_remove[..., 0] = 0
 
-        # Back to unsorted indices and set them to -infinity
-        indices_to_remove = sorted_indices[sorted_indices_to_remove]
-        logits[indices_to_remove] = filter_value
+		# Back to unsorted indices and set them to -infinity
+		indices_to_remove = sorted_indices[sorted_indices_to_remove]
+		logits[indices_to_remove] = filter_value
 
-    indices_to_remove = logits < threshold
-    logits[indices_to_remove] = filter_value
+	indices_to_remove = logits < threshold
+	logits[indices_to_remove] = filter_value
 
-    return logits
+	return logits
 
 
 def sample_sequence(personality, history, tokenizer, model, device, current_output=None):
-    special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
-    if current_output is None:
-        current_output = []
+	special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
+	if current_output is None:
+		current_output = []
 
-    for i in range(20):
-        instance = build_input_from_segments(
-            personality, history, current_output, tokenizer, with_eos=False)
+	for i in range(20):
+		instance = build_input_from_segments(
+			personality, history, current_output, tokenizer, with_eos=False)
 
-        input_ids = torch.tensor(
-            instance["input_ids"], device=device).unsqueeze(0)
-        token_type_ids = torch.tensor(
-            instance["token_type_ids"], device=device).unsqueeze(0)
+		input_ids = torch.tensor(
+			instance["input_ids"], device=device).unsqueeze(0)
+		token_type_ids = torch.tensor(
+			instance["token_type_ids"], device=device).unsqueeze(0)
 
-        logits = model(input_ids, token_type_ids=token_type_ids)
-        if isinstance(logits, tuple):  # for gpt2 and maybe others
-            logits = logits[0]
-        logits = logits[0, -1, :] / 0.7
-        logits = top_filtering(logits, top_k=0, top_p=0.9)
-        probs = F.softmax(logits, dim=-1)
+		logits = model(input_ids, token_type_ids=token_type_ids)
+		if isinstance(logits, tuple):  # for gpt2 and maybe others
+			logits = logits[0]
+		logits = logits[0, -1, :] / 0.7
+		logits = top_filtering(logits, top_k=0, top_p=0.9)
+		probs = F.softmax(logits, dim=-1)
 
-        prev = torch.multinomial(probs, 1)
-        if i < 1 and prev.item() in special_tokens_ids:
-            while prev.item() in special_tokens_ids:
-                if probs.max().item() == 1:
-                    break  # avoid infinitely looping over special token
-                prev = torch.multinomial(probs, num_samples=1)
+		prev = torch.multinomial(probs, 1)
+		if i < 1 and prev.item() in special_tokens_ids:
+			while prev.item() in special_tokens_ids:
+				if probs.max().item() == 1:
+					break  # avoid infinitely looping over special token
+				prev = torch.multinomial(probs, num_samples=1)
 
-        if prev.item() in special_tokens_ids:
-            break
-        current_output.append(prev.item())
+		if prev.item() in special_tokens_ids:
+			break
+		current_output.append(prev.item())
 
-    return current_output
+	return current_output
 
 # personality = [tokenizer.convert_tokens_to_ids(tokenizer.tokenize(persona)) for persona in personality]
 
-def generate_reply(personality, history, tokenizer, model, _device, message):
-    history.append(tokenizer.encode(message))
-    with torch.no_grad():
-        out_ids = sample_sequence(personality, history, tokenizer, model, _device)
-    history.append(out_ids)
-    history = history[-(2*20+1):]
-    out_text = tokenizer.decode(out_ids, skip_special_tokens=True)
-    return out_text
+class ChatBot():
+	def __init__(self):
+		print("[+] Loading pretrained model.. ", end="")
+		self.model_checkpoint = download_pretrained_model()
+		self.tokenizer_class, self.model_class = (OpenAIGPTTokenizer, OpenAIGPTLMHeadModel)
+		self.tokenizer = self.tokenizer_class.from_pretrained(self.model_checkpoint)
+		self.model = self.model_class.from_pretrained(self.model_checkpoint)
+		self._device = "cuda" if torch.cuda.is_available() else "cpu"
+		self.model.to(_device)
+		print("DONE")
+
+		print("[+] Adding special tokens.. ", end="")
+		add_special_tokens_(self.model, self.tokenizer)
+		print("DONE")
+
+		if not os.path.isfile("./dataset_cache" + '_' + type(self.tokenizer).__name__):
+			print("[!] NOTE: THE FOLLOWING PROCESS WILL TAKE SOME TIME.")
+			print("[!] NOTE: DO NOT EXIT THE PROGRAM DURING THE PROCESS.")
+		print("[+] Loading dataset.. ", end="")
+		self.dataset = get_dataset(self.tokenizer, "", "./dataset_cache")
+		print("DONE")
+
+	def generate_reply(self, personality, history, message):
+		history.append(self.tokenizer.encode(message))
+		with torch.no_grad():
+			out_ids = sample_sequence(personality, history, self.tokenizer, self.model, self._device)
+		history.append(out_ids)
+		history = history[-(2*20+1):]
+		return str(self.tokenizer.decode(out_ids, skip_special_tokens=True))
